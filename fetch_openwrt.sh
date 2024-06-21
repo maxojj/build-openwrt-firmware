02342c027c4118770fddf9c4aa260a628ff010d9
maxojj <31129637+maxojj@users.noreply.github.com>
2024-06-21 19:33:07 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-(git clone -b v21.02.5 https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
+(git clone https://github.com/immortalwrt/immortalwrt.git openwrt) || (cd openwrt && git stash && git pull)
 
 cd openwrt
 
-echo "src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git xiaorouji2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 echo "src-git small8 https://github.com/kenzok8/small-package.git;main" >> feeds.conf.default
 
