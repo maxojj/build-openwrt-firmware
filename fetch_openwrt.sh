5944f76a8e7afdbfec42dbf1cd5db72fb0c329ec
maxojj <31129637+maxojj@users.noreply.github.com>
2024-07-07 15:40:38 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -4,7 +4,8 @@
 
 cd openwrt
 
-echo "src-git xiaorouji2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git pw2 https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
+echo "src-git pw2pac https://github.com/xiaorouji/openwrt-passwall-packages.git" >> feeds.conf.default
 #echo "src-git small8 https://github.com/kenzok8/small-package.git;main" >> feeds.conf.default
 
 ./scripts/feeds update -a
